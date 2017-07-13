2ef473a9af25f7a913ec361426d53e49fce9c2a8
prushly <purushothptm@gmail.com>
2017-07-13 19:35:47 +0530

    Add files via upload

---
+++ positiveornegative.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+public class Ideone {
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		
+		int a;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the number");
+		a=s.nextInt();
+		if(a==0){
+			System.out.println("The number is zero");
+		}
+		else if(a<0){
+			System.out.println("The number is negative");}
+			else{
+				System.out.println("The number is positive");
+			}
+		
+		
+		}
+	}
