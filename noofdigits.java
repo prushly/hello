7806bea1d52937961d2f80994f5dd0c14dbf7763
prushly <purushothptm@gmail.com>
2017-07-14 22:38:46 +0530

    Add files via upload

---
+++ noofdigits.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+public class Ideone {
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a;
+		int b=0;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the numer");
+		a=s.nextInt();
+		while(a>0){
+			a=a/10;
+			b++;
+		
+		}
+			System.out.println("The number of digit is " + b);
+			
+			
+		
+		}
+		
+		
+		
+		
+		
+		}
+	
+
+
