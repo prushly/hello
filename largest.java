b9e2c60de83c2d7bdba36f055f0c162f1e9c8db0
prushly <purushothptm@gmail.com>
2017-07-15 14:39:33 +0530

    Add files via upload

---
+++ largest.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+public class Ideone {
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a,b,c;
+		int num;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter first number:");
+		a=s.nextInt();
+		System.out.println("Enter second number:");
+		b=s.nextInt();
+		System.out.println("Enter third number:");
+		c=s.nextInt();
+		num=(a>b)?(a>c?a:c):(b>c?b:c);
+		System.out.println("The largest number among these= "+num);}
+			
+		}
+	
+		
+		
+		
+		
+		
+		
+
+
+
